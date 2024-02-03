5c1d5a253db6f23e0eabcd97e0129bf9d71f250e
sakirmohammad775 <safayetsakirmohammad@gmail.com>
2024-02-03 12:14:56 +0600

    connect with daisyuui and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='text-lime-300 text-3xl'>Vite + React</h1>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/Component/AddCoffee/AddCoffee.jsx
@@ -0,0 +1,10 @@
+
+const AddCoffee = () => {
+    return (
+        <div>
+            <h3>add</h3>
+        </div>
+    );
+};
+
+export default AddCoffee;
